ca0b6695b72828583a556ccc9b5ba388a5024a4e
Adrian Valenzuela <adrianvalenz.web@gmail.com>
2022-04-11 14:38:51 -0700

    fixed type in support file

--- spec/models/client_spec.rb
+++ spec/models/client_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Client, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { described_class.new(
+    name: Faker::Name.name,
+    phone_number: Faker::PhoneNumber.phone_number
+  )}
+
+  describe "Validations" do
+    it "is valid with valid attributes" do
+      expect(subject).to be_valid
+    end
+  end
 end
